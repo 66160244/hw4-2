69834b2db33afe3b41462c33da701ee1fc82700f
66160244 <66160244@go.buu.ac.th>
2025-02-06 00:40:52 +0700

    Finished

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+  extend: {
+  // ส่วนขยายธีมเพิ่มเติมจะอยู่ที่นี่
+  },
+  },
+  plugins: [],
+ }
+ 
+
